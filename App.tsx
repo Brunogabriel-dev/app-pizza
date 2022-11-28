223c84471815b55ad329f939cf75d2b3843efdf0
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-11-28 09:49:32 -0300

    Inclusão de texto

---
+++ App.tsx
@@ -0,0 +1,12 @@
+
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <StatusBar backgroundColor="#1d1d2e" barStyle="light-content" translucent={false}/>
+    </View>
+  );
+}
+
+
