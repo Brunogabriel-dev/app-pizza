af9dfe8ad0c311b3b5f807c6b8bda8ac5e8c76d8
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-11-28 20:19:08 -0300

    Inclusão de texto

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 export default function SignIn(){
   return(
-    <View>
+    <View style={styles.container}>
       <Text>Tela de login</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
