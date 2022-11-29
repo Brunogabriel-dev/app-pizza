2b0e7a070d82949a8097b145cf59b1d38c80614e
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-11-28 21:16:14 -0300

    Inclusão de texto

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native'
+import { 
+  View, 
+  Text, 
+  StyleSheet,
+  Image,
+  TextInput,
+  TouchableOpacity 
+} from 'react-native'
 
 export default function SignIn(){
   return(
     <View style={styles.container}>
-      <Text>Tela de login</Text>
+      <Image
+        style={styles.logo}
+        source={require('../../assets/logo.png')}
+      />
+
+      <View style={styles.inputContainer}>
+        <TextInput
+           placeholder="Digite seu email"
+           style={styles.input}
+           placeholderTextColor="#f0f0f0"
+        />
+        <TextInput
+           placeholder="Sua senha"
+           style={styles.input}
+           placeholderTextColor="#f0f0f0"
+        />
+
+      </View>
+
     </View>
   )
 }
@@ -13,6 +38,26 @@ const styles = StyleSheet.create({
   container:{
     flex:1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    backgroundColor: '#1d1d2e'
+  },
+  logo:{
+    marginBottom: 18
+  },
+  inputContainer:{
+    width: '95%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 32,
+    paddingHorizontal: 14,
+  },
+  input:{
+    width: '95%',
+    height: 40,
+    backgroundColor: '#101026',
+    marginBottom: 12,
+    borderRadius: 4,
+    paddingHorizontal: 8,
+    color: '#fff'
   }
 })
