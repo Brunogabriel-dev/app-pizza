01a47a1292feabca22d0dc3498aaca366e401b7d
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2023-01-17 11:30:05 -0300

    Inclusão de texto

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -2,8 +2,35 @@ import React, {useState, createContext } from "react";
 
 
 type AuthContextData = {
-  
+  user: UserProps;
+  isAuthenticated: boolean;
 }
 
+type UserProps = {
+  id: string;
+  name: string;
+  email: string;
+  token: string
+}
 
-export const AuthContext = createContext({});
+export const AuthContext = createContext({} as AuthContextData);
+
+export function AuthProvider(){
+  const [user, seUser] = useState<UserProps>({
+    id: '',
+    name: '',
+    email: '',
+    token: ''
+  })
+
+  const isAuthenticated = !!user.name;
+
+
+  return(
+    <AuthContext.Provider value={{ user, isAuthenticated }}>
+
+    </AuthContext.Provider>
+
+    
+  )
+}
